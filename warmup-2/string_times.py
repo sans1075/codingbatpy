dd03d7bfb651d7ceca406460b8b0bc46807c7301
sans1075 <148881010+sans1075@users.noreply.github.com>
2024-11-25 07:39:01 -0500

    Create string_times.py

---
+++ warmup-2/string_times.py
@@ -0,0 +1,5 @@
+# Given a string and a non-negative int n, return a larger string that is n copies of the
+# original string.
+
+def string_times(str, n):
+  return str*n # Returns the string n times
