76a2a2e5791802b863cc4b9eb62a2ec5de583977
sans1075 <148881010+sans1075@users.noreply.github.com>
2024-11-25 07:43:09 -0500

    Create string_bits.py

---
+++ warmup-2/string_bits.py
@@ -0,0 +1,5 @@
+# Given a string, return a new string made of every other char starting with the first, so
+# "Hello" yields "Hlo".
+
+def string_bits(str):
+  return str[::2] # Returns every other character of the string using the step parameter
