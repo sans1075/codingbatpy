fabf17faa76ed06ddb0ccec1774202e06ce3eaf5
sans1075 <148881010+sans1075@users.noreply.github.com>
2024-11-25 08:39:52 -0500

    Create extra_end.py

---
+++ string-1/extra_end.py
@@ -0,0 +1,5 @@
+# Given a string, return a new string made of 3 copies of the last 2 chars of the original
+# string. The string length will be at least 2.
+
+def extra_end(str):
+  return 3 * str[-2:] # Returns 3 times the last 2 chars of the string
