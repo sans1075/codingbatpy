7820f8abc1e2788d0c2c56861837ae8152a9b2f8
sans1075 <148881010+sans1075@users.noreply.github.com>
2024-11-27 07:32:58 -0500

    Create near_ten.py

---
+++ logic-1/near_ten.py
@@ -0,0 +1,6 @@
+# Given a non-negative number "num", return True if num is within 2 of a multiple of 10.
+# Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction
+# to Mod
+
+def near_ten(num):
+  return num % 10 <= 2 or num % 10 >= 8 # If the remander is 0, 1, 2, 8, 9, then it is within 2 of a multiple of 10
